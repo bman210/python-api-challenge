1faceb632b415fc4b007bdf0223f41fc7e3e077c
bman210 <bman210@hotmail.com>
2022-11-09 18:36:29 -0500

    Add files via upload

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "4fc8cc178a8224049173678519a2be75"
 
-# Google API Key
-g_key = "YOUR KEY HERE!"
+# Geoapify API Key
+g_key = 'e5a403c480d946c2ae70837dda461f0b'
+
